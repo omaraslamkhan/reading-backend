0545958510562d0c3176a3a68a377d4597cc1863
Omar <umeraslamjs@github.com>
2020-11-02 07:46:20 +0500

    a

--- common/models/notify.js
+++ common/models/notify.js
@@ -1,16 +1,82 @@
 'use strict';
 
+const { notify } = require("../../server/server");
+
 module.exports = function(Notify) {
+  var app = require("../../server/server.js");
+  var request = require('request');
+
+  var fcmConstants = {
+    fcmToken: "AAAASaDQt6Q:APA91bF_aMySfyF0JF7f0y1vdCsZ8xYC2aOIVD4UmQgPEH-qnCl_UtO_ext4_uF31-SeJ8bkySCfJLo31ZscfjxyNTgQZOiS6GNqOjLZM25Tn16qBfHHdg2cIqydbZ-oznAZ2fyzNYpx",
+    senderId: "316230645668",
+    url: "https://fcm.googleapis.com/fcm/send",
+  };
+  let notifyIds=[];
 
 
-    Notify.NotifyInbox = function (userIds, fromUserId, message, cb) {
-        
+
+    Notify.NotifyParent = function (userIds,  message, cb) {
+         
+      var client = app.models.Client;
+        client.find({where: {id: {inq:userIds}}}, function(err, userObjs) {
+          userObjs.forEach((items) => {
+
+            Notify.create({
+              userId: items.id,
+              message:"temporary",
+              type: "temporary",
+
+            },(err,data)=>{
+
+            });
+            if(items.fcmToken==null ||items.fcmToken=='') return
+            notifyIds.push(items.fcmToken);
+            
+          });
+
+          notify(notifyIds);
+          if (err) {
+            cb(err);
+            return;
+          }    
+           cb(null, {status:200});
+          
+          })
        
-      
     }
     
+
+    
+var notify = (notificationUsers) => {
+  var headers = {
+    Authorization: "key=" + fcmConstants.fcmToken,
+    "Content-Type": "application/json",
+    Sender: "id=" + fcmConstants.senderId,
+  };
+  var requestData = {
+    registration_ids: notificationUsers,
+    priority: "high",
+    content_available: true,
+    notification: {
+      title: "Attendance Alert",
+      body: `Attendance alert !!!!`,
+    },
+ 
+  };
+  request.post(
+    {
+      headers: headers,
+      url: fcmConstants.url,
+      body: JSON.stringify(requestData),
+      method: "POST",
+    },
+    function (error, httpResponse, body) {
+      console.log(body);
+    }
+  );
+};
   Notify.remoteMethod('NotifyParent', {
-    accepts: [{arg: 'userId', type: 'string'}, {arg: 'message', type: 'string'}],
+    accepts: [{arg: 'userId', type: 'array'}, {arg: 'message', type: 'string'}],
     returns: {arg: 'result', type: 'string'}
   });
 };
--- common/models/sms.js
+++ common/models/sms.js
@@ -1,19 +1,58 @@
 'use strict';
 
 module.exports = function(Sms) {
+  const Vonage = require('@vonage/server-sdk')
+
+const vonage = new Vonage({
+  apiKey: "b996ceac",
+  apiSecret: "kp3frlyJqXuiGmCz"
+})
     var app = require("../../server/server.js");
     var ObjectID = require("mongodb").ObjectID;
-
-    Sms.SmsParent = function (userIds, fromUserId, message, cb) {
+    Sms.SmsParent = function (userId, message, cb) {
         var client = app.models.Client;
-            client.find({email:userIds}).then((res)=>{
-                console.log(res);
+        client.find({where: {id: {inq:userId}}}, function(err, userObjs) {
+        
+          userObjs.forEach((items)=>{
+            if(items.mobile==null || items.mobile=='') return
+            Sms.sendMessage(items.mobile,null,(err,res)=>{
+              console.log(res);
             })
+          })
 
-      
+         })
+
     }
+
+
+
+
+    Sms.sendMessage = function (receiverId, message, cb) {
+
+      const from = 'Reading Readiness';
+      const to = "+923411235959";
+      const text = 'Attendance testign sms'
+      vonage.message.sendSms(from, to, text, (err, responseData) => {
+        console.log(responseData);
+        if (err) {
+          cb(err, {status:400});
+        } else {
+            if(responseData.messages[0]['status'] === "0") {
+                console.log("Message sent successfully.");
+                cb(null, {status:200});
+              } else {
+                console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+                cb(null, {status:400,error:responseData.messages[0]['error-text']});
+            }
+        }
+    })
+
+      };
+
+
+
     Sms.remoteMethod('SmsParent', {
-        accepts: [{arg: 'userId', type: 'string'}, {arg: 'message', type: 'string'}],
+        accepts: [{arg: 'userId', type: 'array'}, {arg: 'message', type: 'string'}],
         returns: {arg: 'result', type: 'string'}
       });
 };
